2adce899b964ba8a3789a90dad71cbe6aad24548
Khushi Trivedi <khushi.trivedi129697@marwadiuniversity.ac.in>
2026-02-03 08:23:54 +0530

    Add files via upload

---
+++ simpleinterest(source).cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SimpleInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p, r, t, si;
+
+            Console.WriteLine("Enter principal amount:");
+            p = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter rate of interest:");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter number of years:");
+            t = Convert.ToDouble(Console.ReadLine());
+
+            si = (p * r * t) / 100;
+            Console.WriteLine("Simple Interest is: " + si);
+
+            Console.ReadLine();
+        }
+    }
+}
