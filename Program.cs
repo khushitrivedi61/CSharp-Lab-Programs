485a78893324e772d61173a90f2275c7c565fe16
Khushi Trivedi <khushi.trivedi129697@marwadiuniversity.ac.in>
2026-02-03 08:00:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double r, area;
+        Console.WriteLine("Enter the radius of the circle:");
+        r = Convert.ToDouble(Console.ReadLine());
+        area = Math.PI * r * r;
+        Console.WriteLine("Area of the circle is: " + area);
+    }
+}
