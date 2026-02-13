8f13bd153eaeaf993ac74959af3f7ed5d994cd5d
Khushi Trivedi <khushi.trivedi129697@marwadiuniversity.ac.in>
2026-02-13 11:28:03 +0530

    Add files via upload

---
+++ type_conversion2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 100;
+
+        string text = number.ToString();
+
+        Console.WriteLine("Integer value: " + number);
+        Console.WriteLine("String value: " + text);
+    }
+}
