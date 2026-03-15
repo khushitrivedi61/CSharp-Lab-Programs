ca8f7f0f6720a2669f11129a0196a16bb83b426a
Khushi Trivedi <khushi.trivedi129697@marwadiuniversity.ac.in>
2026-03-15 22:53:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n, first = 0, second = 1, next;
+
+        Console.Write("Enter a number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Series:");
+
+        while (first <= n)
+        {
+            Console.Write(first + " ");
+            next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
