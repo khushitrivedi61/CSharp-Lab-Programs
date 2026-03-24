2c53bb5c5ac1c6ce3cb8d74189c1048e38c94307
Khushi Trivedi <khushi.trivedi129697@marwadiuniversity.ac.in>
2026-03-24 07:47:08 +0530

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,64 @@
+﻿// Create a class that contains instance variables such as balance and accNo. Derive two classes from it named Savings and Current.
+// Define appropriate methods in all classes to perform operations like deposit and withdraw for both Savings and Current accounts.
+// Use the concept of inheritance and implement method overriding where required.
+
+using System;
+
+class Account
+{
+    public int accNo;
+    public double balance;
+
+    public virtual void Deposit(double amount)
+    {
+        balance += amount;
+    }
+
+    public virtual void Withdraw(double amount)
+    {
+        balance -= amount;
+    }
+}
+
+class Savings : Account
+{
+    public override void Withdraw(double amount)
+    {
+        if (balance >= amount)
+        {
+            balance -= amount;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient Balance");
+        }
+    }
+}
+
+class Current : Account
+{
+    public override void Withdraw(double amount)
+    {
+        balance -= amount;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Savings s = new Savings();
+        s.accNo = 1;
+        s.balance = 1000;
+        s.Deposit(500);
+        s.Withdraw(300);
+        Console.WriteLine(s.balance);
+
+        Current c = new Current();
+        c.accNo = 2;
+        c.balance = 2000;
+        c.Deposit(1000);
+        c.Withdraw(3500);
+        Console.WriteLine(c.balance);
+    }
+}
---
+++ Base.cs
@@ -0,0 +1,39 @@
+﻿// Create a class that contains instance variables such as var1 and var2. Derive two classes from it named A and B.
+// The A class should include an additional instance variable called extraA, while the B class should include an instance variable called extraB.
+
+using System;
+
+class Base
+{
+    public int var1;
+    public int var2;
+}
+
+class A : Base
+{
+    public int extraA;
+}
+
+class B : Base
+{
+    public int extraB;
+}
+
+class Program
+{
+    static void Main()
+    {
+        A a = new A();
+        a.var1 = 10;
+        a.var2 = 20;
+        a.extraA = 30;
+
+        B b = new B();
+        b.var1 = 40;
+        b.var2 = 50;
+        b.extraB = 60;
+
+        Console.WriteLine(a.var1 + " " + a.var2 + " " + a.extraA);
+        Console.WriteLine(b.var1 + " " + b.var2 + " " + b.extraB);
+    }
+}
